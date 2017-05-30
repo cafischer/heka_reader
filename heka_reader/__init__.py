dbe8bc7c643a3d29c25d2bfac915f2b7865cc971
caro <coralinefischer@gmail.com>
2017-05-30 14:42:50 +0200

    Initial commit

---
+++ heka_reader/__init__.py
@@ -0,0 +1,1 @@
+from custom_hekareader import HekaReader
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='heka_reader',
+      version='0.1',
+      description='Reads Heka .dat files',
+      url='https://github.com/campagnola/heka_reader',
+      author='Luke Campagnola',
+      author_email='luke.campagnola@gmail.com',
+      license='',
+      packages=['heka_reader'],
+      zip_safe=False)
