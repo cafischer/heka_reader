46496e733f9247743be42bfac3eb8258e054dfd1
caro <coralinefischer@gmail.com>
2017-06-29 16:17:32 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='heka_reader',
       version='0.1',
       description='Reads Heka .dat files',
-      url='https://github.com/campagnola/heka_reader',
-      author='Luke Campagnola',
-      author_email='luke.campagnola@gmail.com',
+      url='https://github.com/cafischer/heka_reader',
+      author='Caroline Fischer',
+      author_email='coralinefischer@gmail.com',
       license='',
       packages=['heka_reader'],
       zip_safe=False)
